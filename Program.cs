dd6524f231d88c2fa43748f76c223dc4b359094a
prince kalariya <princekalariya179@gmail.com>
2024-02-09 07:43:42 +0530

    2 : Create DbContext for connect the database.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using Learn_CRUD_CoreWebApp_MVC.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ProviderDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
