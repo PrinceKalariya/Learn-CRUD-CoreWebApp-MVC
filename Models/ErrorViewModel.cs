c5ae287445282a59b7cc72729ecad487e1c3f9ab
princ <princ@PRINCE_KALARIYA>
2024-02-09 06:58:19 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learn_CRUD_CoreWebApp_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
