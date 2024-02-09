748a74aba0976c64e66498ad73dc92e408e9ed4d
prince kalariya <princekalariya179@gmail.com>
2024-02-09 07:27:34 +0530

    1 : Created the model for provider, services and providerservices.

---
+++ Models/Provider.cs
@@ -0,0 +1,13 @@
+﻿namespace Learn_CRUD_CoreWebApp_MVC.Models
+{
+    public class Provider
+    {
+        public int ProviderId { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateOnly DateOfRegistration { get; set; }
+
+        public string IsActive { get; set; }    
+    }
+}
