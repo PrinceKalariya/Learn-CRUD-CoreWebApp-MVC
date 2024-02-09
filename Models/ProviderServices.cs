466acbc3275f10c2ca23c05506a3d9f268002cac
prince kalariya <princekalariya179@gmail.com>
2024-02-09 07:54:03 +0530

    3 : Updated the models by adding Key's.

--- Models/ProviderServices.cs
+++ Models/ProviderServices.cs
@@ -1,11 +1,21 @@
-﻿namespace Learn_CRUD_CoreWebApp_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Learn_CRUD_CoreWebApp_MVC.Models
 {
     public class ProviderServices
     {
+        [Key]
         public int ProviderServiceId { get; set; }
 
         public int ServiceId { get; set; }
 
         public int ProviderId { get; set; }
+
+        [ForeignKey("Provider")]
+        public virtual Provider Provider { get; set; }
+
+        [ForeignKey("Services")]
+        public virtual Services Services { get; set; }
     }
 }
