a205c2dd7144333cb0c49f9e295f650f6759a0b2
prince kalariya <princekalariya179@gmail.com>
2024-02-14 07:41:34 +0530

    9 : Make a Create operation to added the data in databse.

---
+++ ViewModels/ProviderFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Learn_CRUD_CoreWebApp_MVC.Models;
+
+namespace Learn_CRUD_CoreWebApp_MVC.ViewModels
+{
+    public class ProviderFormViewModel
+    {
+        public int ProviderId { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime DateOfRegistration { get; set; }
+
+        public string IsActive { get; set; }
+
+        public int[] ProviderServices { get; set; }
+    }
+}
